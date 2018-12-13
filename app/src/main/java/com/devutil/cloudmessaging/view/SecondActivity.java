0cf3a8b3977fb0407b2af86edaaf6c6c832cf859
Huyen Pham <huyen.phamhoang96@gmail.com>
2018-12-14 00:41:41 +0700

    update

--- app/src/main/java/com/devutil/cloudmessaging/view/SecondActivity.java
+++ app/src/main/java/com/devutil/cloudmessaging/view/SecondActivity.java
@@ -13,17 +13,6 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         String data = getIntent().getStringExtra("data");
-        if (getIntent().getExtras() != null) {
-            for (String key: getIntent().getExtras().keySet()) {
-                if (key.equals("message")) {
-                    Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
-                } else if (key.equals("title")) {
-                    Toast.makeText(this, key, Toast.LENGTH_SHORT).show();
-                } else if (key.equals("body")) {
-                    Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
 
 //        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
     }
